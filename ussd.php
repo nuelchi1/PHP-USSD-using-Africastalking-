d4e3d82ce91093793c51c837d0e4a6df16ce26ed
nuelchi1 <54984583+nuelchi1@users.noreply.github.com>
2019-09-11 19:32:49 +0100

    Update and rename ussd to ussd.php

--- ussd.php
+++ ussd.php
@@ -1,14 +1,19 @@
 <?php
-//This file will be used in africastalking
+//This file will be used for africastalking interactions
 we need to make sure the code runs after a $_POST so we wrap using the IF
 if(!empty($_POST) && !empty($_POST['phoneNumber'])){
 	require_once('connection.php');
 	require_once('AfricasTalkingGateway.php');
 	require_once('config.php');
-	//2. receive the POST from AT
+	// receive the POST from AfricasTalking
 	$sessionId     =$_POST['sessionId'];
 	$serviceCode   =$_POST['serviceCode'];
 	$phoneNumber   =$_POST['phoneNumber'];
 	$text          =$_POST['text'];
+	// since we need to correctly display next level we will explode the value of the user interaction- think 1*1
+	$textArray=explode('*', $text);
+	$userResponse=trim(end($textArray));
+	// we then need to set the default level so as to conveniently get the main page
+	$level=0;
 
 ?>
